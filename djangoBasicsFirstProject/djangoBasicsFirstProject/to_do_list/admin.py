bc55282028a8f79c3168362cda98b71ad447e296
Nikolay Toshev <n.toshev@proton.me>
2024-09-16 16:56:02 +0300

    modified first and second project

--- djangoBasicsFirstProject/djangoBasicsFirstProject/to_do_list/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from djangoBasicsFirstProject.to_do_list.models import Todo
-
-
-# Register your models here.
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    pass
--- djangoBasicsFirstProject/djangoBasicsFirstProject/to_do_list/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from djangoBasicsFirstProject.to_do_list.views import list_of_tasks
-
-urlpatterns = [
-    path('', list_of_tasks)
-]
