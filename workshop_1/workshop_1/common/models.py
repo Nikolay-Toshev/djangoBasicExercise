030aa1607d7d4f606513cdfd9e70caf189f6d824
Nikolay Toshev <n.toshev@proton.me>
2024-09-21 23:48:50 +0300

    added new models to workshop1

--- workshop_1/workshop_1/common/models.py
+++ workshop_1/workshop_1/common/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from workshop_1.photos.models import Photo
 
-# Create your models here.
+class Comment(models.Model):
+    text = models.TextField(max_length=300)
+    date_of_publication = models.DateTimeField(auto_now_add=True)
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
--- workshop_1/workshop_1/photos/models.py
+++ workshop_1/workshop_1/photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from workshop_1.pets.models import Pet
+from workshop_1.photos.validators import validate_file_size
 
-# Create your models here.
+class Photo(models.Model):
+    photo = models.ImageField(validators=(validate_file_size, ))
+    description = models.TextField(max_length=300, validators=[MinValueValidator(10)], blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
+    tagged_pets = models.ManyToManyField(Pet, blank=True)
+    date_of_publication = models.DateField(auto_now=True)
